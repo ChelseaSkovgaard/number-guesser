63d42b8e9cd614267c1047f79c89e7aa7cf2505f
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2016-08-23 19:28:44 -0600

    added clear button

--- scripts.js
+++ scripts.js
@@ -2,16 +2,15 @@ var message = document.querySelector('.message');
 //
 // var mostRecentGuess = document.querySelector('.most-recent');
 //
-// var clearInput = document.querySelector('.clear-input');
+var clearInputButton = document.querySelector('.clear-input');
 //
-// var resetGame = document.querySelector('.reset-game');
+var resetGameButton = document.querySelector('.reset-game');
 
-// var getRandomNumber = Math.floor( Math.random() * 100) + 1;
-var getRandomNumber = 10;
+var getRandomNumber = Math.floor( Math.random() * 100) + 1;
+
+// var getRandomNumber = 10;
 
 var guessInput = document.getElementById('number-guess');
-
-
 
 var submitGuessButton = document.querySelector('.submit-guess');
 
@@ -21,23 +20,34 @@ function submitEvent(){
   console.log(numberGuessed);
   if (getRandomNumber === numberGuessed) {
     message.innerText = "Yay! You are Right";
-  } else {
-    message.innerText = "Nope";
   }
 
-  // if (getRandomNumber < guessInputValue) {
-  //   message.innerText = "You're guess is too high";
-  // }
-  //
-  // if (getRandomNumber > guessInputValue) {
-  //   message.innerText = "You're guess is too low";
-  // }
+if (getRandomNumber < numberGuessed) {
+   message.innerText = "You're guess is too high";
+  }
+  if (getRandomNumber > numberGuessed) {
+    message.innerText = "You're guess is too low";
+   }
 }
 
 submitGuessButton.addEventListener('click', function(e) {
-  debugger;
   submitEvent();
 });
 
+function clearInput() {
+  guessInput.value="";
+}
+
+clearInputButton.addEventListener('click', function(e) {
+  clearInput();
+});
+
+function resetGame () {
+  guessInput.value="";
+}
+
+resetGameButton.addEventListener('click', function(e) {
+  resetGame();
+});
 //
 // e.preventDefault --> Stops buttons from doing the things. e is inside the function.
