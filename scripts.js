8530459c462438024c9659aaa23ae0566c95b38e
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2016-08-24 20:08:52 -0600

    level 2 with some CSS styling and cleaned up javascript

--- scripts.js
+++ scripts.js
@@ -1,25 +1,22 @@
-var message = document.querySelector('.message');
+var guessInput = document.getElementById('number-guess');
 
-var lastGuess = document.querySelector('.last-guess');
+var randomNumber = getRandomNumber();
+
+var submitGuessButton = document.querySelector('.submit-guess');
 
 var clearInputButton = document.querySelector('.clear-input');
 
 var resetGameButton = document.querySelector('.reset-game');
 
+var lastGuess = document.querySelector('.last-guess');
+
+var message = document.querySelector('.message');
+
 var messageRange = document.querySelector('.range-error');
 
-var guessInput = document.getElementById('number-guess');
-
-var submitGuessButton = document.querySelector('.submit-guess');
-
-var submitRangeButton = document.querySelector('.submit-range');
-
-// this is the problem!
-function getRandomNumber (min, max) {
-  debugger;
-  return (Math.floor(Math.random() * (max-min + 1)) + min);
+function getRandomNumber() {
+  return Math.floor( Math.random() * 100) + 1;
 }
-
 
 function disableClearButton() {
   clearInputButton.disabled = true;
@@ -29,6 +26,8 @@ function enableClearButton(){
   clearInputButton.disabled = false;
 }
 
+disableClearButton();
+
 function disableResetButton() {
   resetGameButton.disabled = true;
 }
@@ -36,54 +35,28 @@ function disableResetButton() {
 function enableResetButton() {
   resetGameButton.disabled = false;
 }
-disableClearButton();
 
 disableResetButton();
 
-
-var min = document.querySelector('.min');
-var max = document.querySelector('.max');
-var userMin = document.querySelector('.min-range');
-var userMax = document.querySelector('.max-range');
-
-function submitRange () {
-  userMin.innerText = "Your minimum number is: " + min.value;
-  userMax.innerText = "Your maximum number is: " + max.value;
-  // puts min and max in dom somewhere
-}
-
-submitRangeButton.addEventListener('click', function(e) {
-  submitRange();
-});
-
 function submitEvent(){
-
   var numberGuessed = parseInt(guessInput.value);
-  var randomNumber = getRandomNumber(parseInt(min.value), parseInt(max.value));
-  // var min = userinput min in dom
-  // var max = userinput max in dom
-  console.log(randomNumber);
-  console.log(numberGuessed);
   if (randomNumber === numberGuessed) {
-    message.innerText = "Yay! You are Right";
+    message.innerText = "Yay! You guessed the right number!";
   }
   if (randomNumber < numberGuessed) {
-   message.innerText = "You're guess is too high";
+   message.innerText = "Your guess is too high.";
   }
   if (randomNumber > numberGuessed) {
-    message.innerText = "You're guess is too low";
+    message.innerText = "Your guess is too low.";
    }
-  //  if (numberGuessed === 'NaN') {
-  //    lastGuess.innerText = "Error - that is not a number!";
-  //  }
 
    lastGuess.innerText = numberGuessed;
 
-   if (numberGuessed >= max || numberGuessed <= min) {
-     messageRange.innerText = "You're number is not between 1 and 100.";
+   if (numberGuessed >= 100 || numberGuessed <= 1) {
+     messageRange.innerText = "Your number is not between 1 and 100.";
    }
 
-   if (numberGuessed <= max && numberGuessed >=min) {
+   if (numberGuessed <= 100 && numberGuessed >=1) {
      messageRange.innerText ="";
    }
    enableClearButton();
@@ -109,6 +82,3 @@ function gameReset (){
 resetGameButton.addEventListener('click', function(e) {
   gameReset();
 });
-
-//
-// e.preventDefault --> Stops buttons from doing the things. e is inside the function.
